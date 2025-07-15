feff3bc0db129ea6dc92233b4c6bd7501e63af3f
sarthakpawar0912 <pawarsr06@gmail.com>
2025-07-15 09:58:14 +0530

    Fixed bugs and updated code

--- src/InterfaceWithLambdaFunction/Main2.java
+++ src/InterfaceWithLambdaFunction/Main2.java
@@ -1,19 +1,20 @@
 package InterfaceWithLambdaFunction;
 
 public class Main2 {
+
     public static void main(String[] args) {
-        // Lambda expression for t1 to add three numbers
+
         Test t1 = (a, b, c) -> a + b + c;
 
-        // Lambda expression for t2 to multiply three numbers
+
         Test t2 = (a, b, c) -> a * b * c;
 
-        // Test with values 2, 3, 4
-        int result1 = t1.myFunction(2, 3, 4);
+
+         int result1 = t1.myFunction(2, 3, 4);
         int result2 = t2.myFunction(2, 3, 4);
 
-        // Print results
         System.out.println("Addition result: " + result1); // Should print 9
         System.out.println("Multiplication result: " + result2); // Should print 24
+
     }
 }
