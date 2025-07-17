8293259f710dd49886920b54c79ca91539557c4d
sarthakpawar0912 <pawarsr06@gmail.com>
2025-07-17 17:46:07 +0530

    Fixed bugs and updated code

--- src/MusicApplication/Saxophone.java
+++ src/MusicApplication/Saxophone.java
@@ -1,8 +1,10 @@
 package MusicApplication;
 
 public class Saxophone implements Playable {
+
     @Override
     public void play() {
+
         System.out.println("Playing Saxophone...");
     }
 }
