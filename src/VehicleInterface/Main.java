db36073493449493b0133ef89a32699257e02d66
sarthakpawar0912 <pawarsr06@gmail.com>
2025-07-17 17:44:27 +0530

    Fixed bugs and updated code

--- src/VehicleInterface/Main.java
+++ src/VehicleInterface/Main.java
@@ -1,7 +1,9 @@
 package VehicleInterface;
 
  public class Main {
+
         public static void main(String[] args) {
+
             VehicleImpl.Car car = new VehicleImpl.Car();
             car.message();
         }
