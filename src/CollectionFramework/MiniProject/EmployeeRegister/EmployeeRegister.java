8181bbfc9f5ade81ade397ac07f735f56fa0b047
sarthakpawar0912 <pawarsr06@gmail.com>
2025-07-21 10:32:23 +0530

    Fixed bugs and updated code

--- src/CollectionFramework/MiniProject/EmployeeRegister/EmployeeRegister.java
+++ src/CollectionFramework/MiniProject/EmployeeRegister/EmployeeRegister.java
@@ -40,6 +40,5 @@ public class EmployeeRegister {
             System.out.format("%-15s %-15s %-15s %-30s %-45s%n", emp.firstName, emp.lastName, emp.mobileNumber, emp.email, emp.address);
         }
 
-
     }
 }
