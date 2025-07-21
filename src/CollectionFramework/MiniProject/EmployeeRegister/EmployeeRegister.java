4185e8ce4fa034001a5a26e076ffa1daf14446d9
sarthakpawar0912 <pawarsr06@gmail.com>
2025-07-21 10:34:18 +0530

    Fixed bugs and updated code

--- src/CollectionFramework/MiniProject/EmployeeRegister/EmployeeRegister.java
+++ src/CollectionFramework/MiniProject/EmployeeRegister/EmployeeRegister.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class EmployeeRegister {
-    public static void main(String[] args) {
+public class EmployeeRegister { public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
