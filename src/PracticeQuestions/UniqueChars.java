1d23e9c58a366203cb6b53c980160b75e281d0a6
sarthakpawar0912 <pawarsr06@gmail.com>
2025-07-30 09:39:46 +0530

    Fixed bugs and updated code

--- src/PracticeQuestions/UniqueChars.java
+++ src/PracticeQuestions/UniqueChars.java
@@ -5,8 +5,11 @@ public class UniqueChars {
     public static void printUniqueChars(String str) {
 
         for (int i = 0; i < str.length(); i++) {
+
             boolean isUnique = true;
+
             for (int j = 0; j < str.length(); j++) {
+
                 if (i != j && str.charAt(i) == str.charAt(j)) {
                     isUnique = false;
                     break;
